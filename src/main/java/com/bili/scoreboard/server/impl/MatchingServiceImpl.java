60314a17ddaf2a270f0f7e271c6d5625c630ceeb
bilikings <39830081+bilikings@users.noreply.github.com>
2021-01-20 17:47:33 +0800

    init

--- src/main/java/com/bili/scoreboard/server/impl/MatchingServiceImpl.java
+++ src/main/java/com/bili/scoreboard/server/impl/MatchingServiceImpl.java
@@ -3,9 +3,12 @@ package com.bili.scoreboard.server.impl;
 import com.bili.scoreboard.server.MatchingService;
 import org.springframework.stereotype.Service;
 
-/**
- * @author oxx
- */
+ /**
+ 　　* @description: TODO
+ 　　* @throws
+ 　　* @author oxx
+ 　　* @date 2021/1/20 17:46
+ 　　*/
 @Service("matchingService")
 public class MatchingServiceImpl implements MatchingService {
 }
